a83e76c21ccfd87e7235c515578385a328cf2b7b
alannxq <85573908+alannxq@users.noreply.github.com>
2022-03-23 11:16:37 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,28 @@
-## it should be very easy to add more sorting algs, ill probably add a few new ones soon.
-## just pass "numbers" list, and make sure to update what is on screen using:
-
-#  drawBars(numbers)
-#  turtle.update()
-#  time.sleep(SPEED)
-
 import turtle
 import random
 import time
 
-bar = turtle.Turtle()
-bar.hideturtle()
-turtle.tracer(0, 0) ## with speed(0) its slow
-
-NUM_BARS = 100 ## changing this also changed width of bars
-SPEED = 0.01 ## the lower the faster
-
-WIDTH = 800 / NUM_BARS
-START_POS = -((WIDTH * NUM_BARS) / 2), 0
-
-numbers = [random.randint(20,200) for _ in range(NUM_BARS)]
 
 
 def drawBars(nums):
+	bar.color("black")
+	bar.fillcolor("red")
 	bar.begin_fill()
 	bar.clear() ## clear previous drawings
 	bar.penup()
 	bar.goto(START_POS) ## go to start
 	bar.pendown()
 	bar.seth(90) ## point up // set-h(eading)
-	
+
 	for HEIGHT in nums:
 		bar.forward(HEIGHT) ## left side of bar
 		bar.right(90)
-		bar.forward(WIDTH) ## draw top of bar
+		bar.forward(WIDTH) ## draw top
 		bar.right(90)
 		bar.forward(HEIGHT) ## go back down, right side of bar
-		bar.left(180) ## after going down, 180 turn. ready for new bar to be drawn
+		bar.left(180) ## after going down, turn around for next bar
+
+	bar.end_fill()
 
 
 def bubbleSort(arr):
@@ -45,9 +31,68 @@ def bubbleSort(arr):
 			if numbers[index] > numbers[index + 1]:
 				numbers[index], numbers[index + 1] = numbers[index + 1], numbers[index]
 			
-			## draw updated bar chart
 			drawBars(numbers)
 			turtle.update()
 			time.sleep(SPEED)
 
-bubbleSort(numbers)
+
+def selection_sort(A):
+	for i in range(len(A)):
+		min_idx = i
+		for j in range(i+1, len(A)):
+			if A[min_idx] > A[j]:
+				min_idx = j
+	                    
+		A[i], A[min_idx] = A[min_idx], A[i]
+
+		drawBars(numbers)
+		turtle.update()
+		time.sleep(SPEED)
+
+
+def insertionSort(arr):
+
+	for i in range(1, len(arr)):
+		key = arr[i]
+
+		j = i-1
+		while j >=0 and key < arr[j] :
+			arr[j+1] = arr[j]
+			j -= 1
+			drawBars(numbers)
+			turtle.update()
+			time.sleep(SPEED)
+
+		arr[j+1] = key
+
+		drawBars(numbers)
+		turtle.update()
+		time.sleep(SPEED)
+
+
+sort = input("a) bubble sort\nb) selection sort\nc) insertion sort\n\n> ")
+NUM_BARS = int(input("\n\nEnter amount of bars to sort: "))
+
+
+numbers = [random.randint(20,200) for _ in range(NUM_BARS)]
+
+SPEED = 0.01 ## the lower the faster
+
+WIDTH = 800 / NUM_BARS
+START_POS = -((WIDTH * NUM_BARS) / 2), 0
+
+if sort == "a":
+	bar = turtle.Turtle()
+	bar.hideturtle()
+	turtle.tracer(0, 0)
+	bubbleSort(numbers)
+elif sort == "b":
+	bar = turtle.Turtle()
+	bar.hideturtle()
+	turtle.tracer(0, 0)
+	selection_sort(numbers)
+else:
+	bar = turtle.Turtle()
+	bar.hideturtle()
+	turtle.tracer(0, 0)
+	insertionSort(numbers)
